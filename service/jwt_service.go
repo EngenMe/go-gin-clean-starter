91ef369fa1e9928972cca4098db69b2b5a7ca414
AnesRabai <anesrabai202@gmail.com>
2025-04-29 14:04:25 +0100

    Implement GetRoleByToken and GenerateAccessToken methods in JWTService

--- service/jwt_service.go
+++ service/jwt_service.go
@@ -13,6 +13,8 @@ type JWTService interface {
 	GenerateToken(userId string, role string) string
 	ValidateToken(token string) (*jwt.Token, error)
 	GetUserIDByToken(token string) (string, error)
+	GetRoleByToken(token string) (string, error)
+	GenerateAccessToken(userID string, role string) (string, int64, error)
 }
 
 type jwtCustomClaim struct {
@@ -76,8 +78,39 @@ func (j *jwtService) GetUserIDByToken(token string) (string, error) {
 	if err != nil {
 		return "", err
 	}
-	
+
 	claims := t_Token.Claims.(jwt.MapClaims)
 	id := fmt.Sprintf("%v", claims["user_id"])
 	return id, nil
+}
+func (j *jwtService) GetRoleByToken(token string) (string, error) {
+	t_, err := j.ValidateToken(token)
+	if err != nil {
+		return "", err
+	}
+
+	claims := t_.Claims.(jwt.MapClaims)
+	role := claims["role"].(string)
+	return role, nil
+}
+
+func (j *jwtService) GenerateAccessToken(userID string, role string) (string, int64, error) {
+	expirationTime := time.Now().Add(time.Minute * 15)
+	expiresIn := int64(expirationTime.Sub(time.Now()).Seconds())
+
+	claims := &jwtCustomClaim{
+		UserID: userID,
+		Role:   role,
+		RegisteredClaims: jwt.RegisteredClaims{
+			ExpiresAt: jwt.NewNumericDate(expirationTime),
+			Issuer:    j.issuer,
+			IssuedAt:  jwt.NewNumericDate(time.Now()),
+		},
+	}
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	t, err := token.SignedString([]byte(j.secretKey))
+	if err != nil {
+		return "", 0, err
+	}
+	return t, expiresIn, nil
 }
